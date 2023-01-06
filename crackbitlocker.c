e9cac2957ca2937e48817202ce3bf3934315c447
LAMRI Djamal <djamallamri@yahoo.fr>
2023-01-06 22:19:24 +0100

    correction

--- crackbitlocker.c
+++ crackbitlocker.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <stdio.h>
 
 #define MAX_PASS_LENGTH 18
@@ -11,10 +12,18 @@ const char char_set[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                             'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
                             'U', 'V', 'W', 'X', 'Y', 'Z'};
 
-int main(void)
+int main(int argc, char **argv)
 {
     char pass[MAX_PASS_LENGTH + 1];
     int i;
+    char *disk;
+
+    if(argc < 2) {
+        fprintf(stderr, "Veuillez spécifier un disque\n");
+        return 1;
+    }
+
+    disk = argv[1];
 
     /* Générer toutes les combinaisons possibles de mots de passe en commencer par 4 caractères*/
     for(i = 0; i < CHAR_SET_SIZE; i++) {
@@ -26,7 +35,7 @@ int main(void)
 
         while(strlen(pass) <= MAX_PASS_LENGTH) {
             /* Tenter de déverrouiller le disque avec le mot de passe actuel en specifiant le disk */
-            if(unlock_disk_with_password("C:\\", pass)) {
+            if(unlock_disk_with_password(disk, pass)) {
                 printf("Le mot de passe est : %s\n", pass);
                 return 0;
             }
@@ -38,4 +47,3 @@ int main(void)
     printf("Aucun mot de passe trouvé\n");
     return 1;
 }
-
